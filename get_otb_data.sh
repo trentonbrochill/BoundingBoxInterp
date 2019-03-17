a48dd815622c96a02cc6f9a437d14ba47900ea7c
Matthew Schnur <matthew.p.schnur@gmail.com>
2019-03-17 17:08:31 -0400

    Make get_otb_data more parallel

--- get_otb_data.sh
+++ get_otb_data.sh
@@ -4,7 +4,7 @@ mkdir OTB2015 && cd OTB2015
 baseurl="http://cvlab.hanyang.ac.kr/tracker_benchmark"
 wget "$baseurl/datasets.html"
 cat datasets.html | grep '\.zip' | sed -e 's/\.zip".*/.zip/' | sed -e s'/.*"//' >files.txt
-cat files.txt | xargs -n 1 -P 8 -I {} wget -c "$baseurl/{}"
-ls *.zip | xargs -n 1 unzip
+cat files.txt | xargs -n 1 -P 16 -I {} wget -c "$baseurl/{}"
+ls *.zip | xargs -n 1 -P 16 unzip
 rm -r __MACOSX/
 cd ..
