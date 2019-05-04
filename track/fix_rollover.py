d922f323a7ad483bf48103248bfd00aab1bdfcb8
Matt Schnur <matthew.p.schnur@gmail.com>
2019-05-04 13:25:53 -0400

    Add option to fix rollover in one manually specified file

--- track/fix_rollover.py
+++ track/fix_rollover.py
@@ -50,45 +50,52 @@ def detect_and_fix_rollover(file_path):
 def main():
     # base dataset path and setting
     parser = argparse.ArgumentParser(description='Fix rollover by setting it equal to the max val')
-    parser.add_argument('--video_output_folder', metavar='FOLDER', required=True,
-                        help='Path of folder to write all generated heatmaps for videos in dataset_folder')
+    group = parser.add_mutually_exclusive_group(required=True)
+    group.add_argument('--video_output_folder', metavar='FOLDER',
+                       help='Path of folder to read/write heatmap data (should be root folder containing bb_h=A_w=B folders')
+    group.add_argument('--heatmap_data_file', metavar='FILE',
+                       help='Specify this instead of the video output folder in order to run on only a single, manually specified heatmap data image')
     args = parser.parse_args()
 
-    abs_video_output_folder = os.path.realpath(args.output_folder)
-    print "abs_video_output_folder:", abs_video_output_folder
-    for bb_h_w_dir_entry in os.listdir(abs_video_output_folder):
-        print "bb_h_w_dir_entry:", bb_h_w_dir_entry
-        bb_h_w_dir = os.path.join(abs_dataset_folder, bb_h_w_dir_entry)
-        if not os.path.isdir(bb_h_w_dir):
-            print "not directory:", bb_h_w_dir
-            continue
-
-        if BB_HEIGHT_WIDTH_DIRECTORY_REGEX.search(bb_h_w_dir_entry) is None:
-            print "Doesn't match bb Height/Width directory regex:", bb_h_w_dir_entry
-            continue
-
-        for target_image_dir_entry in os.listdir(bb_h_w_dir):
-            target_image_dir = os.path.join(bb_h_w_dir, target_image_dir_entry)
-            if not os.path.isdir(target_image_dir):
-                print "not directory:", target_image_dir
+    if args.video_output_folder is not None:
+        abs_video_output_folder = os.path.realpath(args.output_folder)
+        print "abs_video_output_folder:", abs_video_output_folder
+        for bb_h_w_dir_entry in os.listdir(abs_video_output_folder):
+            print "bb_h_w_dir_entry:", bb_h_w_dir_entry
+            bb_h_w_dir = os.path.join(abs_dataset_folder, bb_h_w_dir_entry)
+            if not os.path.isdir(bb_h_w_dir):
+                print "not directory:", bb_h_w_dir
                 continue
 
-            if TARGET_IMAGE_DIRECTORY_REGEX.search(target_image_dir_entry) is None:
-                print "Doesn't match target image directory regex:", target_image_dir_entry
+            if BB_HEIGHT_WIDTH_DIRECTORY_REGEX.search(bb_h_w_dir_entry) is None:
+                print "Doesn't match bb Height/Width directory regex:", bb_h_w_dir_entry
                 continue
 
-            heatmap_data_dir = os.path.join(target_image_dir, "heatmap_data")
-            for frame_file_dir_entry in os.listdir(heatmap_data_dir):
-                frame_file = os.path.join(heatmap_data_dir, frame_file_dir_entry)
-                if not os.path.isfile(frame_file):
-                    print "not file:", frame_file
+            for target_image_dir_entry in os.listdir(bb_h_w_dir):
+                target_image_dir = os.path.join(bb_h_w_dir, target_image_dir_entry)
+                if not os.path.isdir(target_image_dir):
+                    print "not directory:", target_image_dir
                     continue
 
-                if FRAME_FILE_REGEX.search(frame_file_dir_entry) is None:
-                    print "Doesn't match frame file regex:", frame_file_dir_entry
+                if TARGET_IMAGE_DIRECTORY_REGEX.search(target_image_dir_entry) is None:
+                    print "Doesn't match target image directory regex:", target_image_dir_entry
                     continue
 
-                detect_and_fix_rollover(frame_file)
+                heatmap_data_dir = os.path.join(target_image_dir, "heatmap_data")
+                for frame_file_dir_entry in os.listdir(heatmap_data_dir):
+                    frame_file = os.path.join(heatmap_data_dir, frame_file_dir_entry)
+                    if not os.path.isfile(frame_file):
+                        print "not file:", frame_file
+                        continue
+
+                    if FRAME_FILE_REGEX.search(frame_file_dir_entry) is None:
+                        print "Doesn't match frame file regex:", frame_file_dir_entry
+                        continue
+
+                    detect_and_fix_rollover(frame_file)
+    else:
+        abs_frame_file = os.path.realpath(args.heatmap_data_file)
+        detect_and_fix_rollover(abs_frame_file)
                 
                 
 if __name__ == '__main__':
